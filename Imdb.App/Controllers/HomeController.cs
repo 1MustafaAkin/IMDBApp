3bbd38acc9b380b0665d56eb66c374a7bc2b61e3
muxty <1mustafa.akin@gmail.com>
2019-02-28 04:09:57 +0200

    Repository Desing Pattern entegre edildi. Dependency Injection ile gerekli iyileştirmeler yapıldı .Anasayfa tasarlandı. Layout hazırlandı.

---
+++ Imdb.App/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Imdb.DAL;
+using Imdb.DATA.Concrete;
+
+namespace Imdb.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Context db = new Context();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.MoviesSeries.ToList());
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MoviesSeries moviesSeries = db.MoviesSeries.Find(id);
+            if (moviesSeries == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moviesSeries);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Imdb.App/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Imdb.App.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(FormCollection frm)
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection frm)
+        {
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
